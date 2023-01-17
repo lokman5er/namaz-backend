d7aada7ee9d5ca6bbdf985f3e1fb956073c755bd
lokman5er <lokmanbeser@icloud.com>
2023-01-17 17:04:45 +0100

    fix cors error

--- server.js
+++ server.js
@@ -40,13 +40,21 @@ mongoose.connect(
     }
 );
 
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 
 app.post('/api/register', async (req, res) => {
     const { username, password: plainTextPassword, urlPara, apiKey } = req.body
