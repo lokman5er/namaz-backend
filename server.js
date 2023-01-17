ca823eb9aef734d34a7c5ca046ba36271886fd72
lokman5er <lokmanbeser@icloud.com>
2023-01-17 17:41:46 +0100

    fix

--- server.js
+++ server.js
@@ -41,11 +41,10 @@ mongoose.connect(
 );
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     if (req.method === "OPTIONS") {
         res.header(
             "Access-Control-Allow-Methods",
