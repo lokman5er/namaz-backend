b24f8d66822f94f4e468654e5471f4ecbfce4f26
lokman5er <lokmanbeser@icloud.com>
2023-01-17 17:36:57 +0100

    bug fix

--- server.js
+++ server.js
@@ -40,14 +40,23 @@ mongoose.connect(
     }
 );
 
-const cors = require("cors");
-const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
 
-app.use(cors(corsOptions))
+
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
