67203bde6d038de3bfe7086e37efb0c44d8b5b4a
lokman5er <lokmanbeser@icloud.com>
2023-01-17 17:49:11 +0100

    fix

--- server.js
+++ server.js
@@ -40,12 +40,10 @@ mongoose.connect(
     }
 );
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-})
+var cors = require('cors')
+
+app.use(cors())
+
 
 
 app.use('/', express.static(path.join(__dirname, 'static')))
