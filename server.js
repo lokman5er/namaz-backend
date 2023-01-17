1ea2ba9a88c4eb382d5c31295290e8089ddb94b3
lokman5er <lokmanbeser@icloud.com>
2023-01-17 17:28:43 +0100

    cors fix

--- server.js
+++ server.js
@@ -50,19 +50,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// })
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 app.post('/api/register', async (req, res) => {
     const { username, password: plainTextPassword, urlPara, apiKey } = req.body
